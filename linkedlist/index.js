d8592e518bd6969aa47cedd61f98ce1c1d067b9e
Ritik <iritikdev@gmail.com>
2023-04-14 13:00:17 +0530

    Implement push() functionality

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -11,4 +11,14 @@ class LinkedList {
     this.last = null;
     this.length = 0;
   }
+  push(item) {
+    const node = new Node(item);
+    if (this.first === null) this.first = this.last = node;
+    else {
+      this.last.next = node;
+      this.last = node;
+    }
+
+    this.length++;
+  }
 }
